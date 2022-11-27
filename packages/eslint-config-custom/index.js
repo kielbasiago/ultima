c89c277124b06ed2f72ed26ef61aa47b6051f8f5
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-11-27 13:22:44 -0500

    fix linting

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["next", "next/babel", "turbo", "prettier"],
+  extends: ["next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",

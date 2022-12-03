cbd066238043f695db1260de75dab579134a5ceb
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-12-03 18:48:21 -0500

    invoke Arguments with -i

--- apps/balance-and-ruin/api/metadata/flag.py
+++ apps/balance-and-ruin/api/metadata/flag.py
@@ -6,13 +6,13 @@ class handler(BaseHTTPRequestHandler):
   def do_GET(self):    
     sys.path.append("WorldsCollide")
     from WorldsCollide.arguments import Arguments
-    arguments = Arguments(False)
+    arguments = Arguments(args = ['-i', 'ff3.smc'])
 
     from WorldsCollide.metadata.flag_metadata_writer import FlagMetadataWriter
 
     result = FlagMetadataWriter(arguments).get_flag_metadata()
     import json
     self.send_response(200)
-    self.send_header('Content-type','text/plain')
+    self.send_header('Content-type','text/plain') 
     self.end_headers()
     self.wfile.write(json.dumps(result).encode())

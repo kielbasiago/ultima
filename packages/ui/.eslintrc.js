0005cd66f58d0fe20ca5ac2c6ad0f3e4208cb20f
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-12-04 09:52:12 -0500

    chore: remove ui package

--- packages/ui/.eslintrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ["@ff6wc/eslint-config-custom"],
-};
--- packages/ui/components/FormLabel/FormLabel.tsx
+++
@@ -1,8 +0,0 @@
-export type FormLabelProps = Record<string, unknown>;
-
-const FormLabel = (props: FormLabelProps) => {
-  const {} = props;
-  return <></>;
-};
-
-export default FormLabel;

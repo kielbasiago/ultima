cbd9aa09e53c15ddac61a3e79b950703d3ec5d1a
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-12-05 21:59:00 -0500

    add print

--- apps/balance-and-ruin/api/generate-rom.py
+++ apps/balance-and-ruin/api/generate-rom.py
@@ -1,17 +1,17 @@
 from http.server import BaseHTTPRequestHandler
-import io
-from os import sendfile
 import subprocess
 import tempfile
 import ips
 
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):    
+    print('inside handler')
     import sys
     sys.path.append("WorldsCollide")
     from WorldsCollide.wc import main
     with tempfile.TemporaryDirectory() as temp_dir:
         out_filename = temp_dir + "ff3-out.smc"
+        print('output filename', out_filename)
         result = subprocess.Popen(['python', 'WorldsCollide/wc.py', '-i', 'ff3.smc', '-o', out_filename]).wait()
         print('result', result)
         with open("ff3.smc", "rb") as old, open(out_filename, "rb") as new:

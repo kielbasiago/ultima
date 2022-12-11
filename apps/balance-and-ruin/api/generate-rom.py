380063d99bbee1fb157145a5b339792eb14285cd
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-12-11 13:18:11 -0500

    Update proper uses of in_filename

--- apps/balance-and-ruin/api/generate-rom.py
+++ apps/balance-and-ruin/api/generate-rom.py
@@ -14,44 +14,30 @@ class handler(BaseHTTPRequestHandler):
 
     with tempfile.TemporaryDirectory() as temp_dir:
         in_filename = temp_dir + "ff3.smc"
+        out_filename = temp_dir + "ff3-out.smc"
 
         src_file = os.getenv("INPUT_ROM") or 'ff3.smc'
         
-        print('using src file', src_file)
-
         if src_file.startswith('http'):
           urllib.request.urlretrieve(src_file, in_filename)
-          print('request finished in_filename', in_filename)
         else:
           shutil.copyfile(src_file, in_filename)
-          print('copy src_file into in_filename')
             
-        out_filename = temp_dir + "ff3-out.smc"
-        result = subprocess.Popen(['python', 'WorldsCollide/wc.py', '-i', 'ff3.smc', '-o', out_filename, '-s', 'foo']).wait()
+        result = subprocess.Popen(['python', 'WorldsCollide/wc.py', '-i', in_filename, '-o', out_filename, '-s', 'foo']).wait()
         
-        # result is the response code from running the sub process
+        # result is the return code from running the sub process
         if result:
           self.send_response(400)
           self.send_header('Content-type', 'text/plain')
           self.end_headers()
           self.wfile.write()
         else:
-          with open(src_file, "rb") as old, open(out_filename, "rb") as new:
+          with open(in_filename, "rb") as old, open(out_filename, "rb") as new:
               import base64
-              import json
-              from io import BytesIO
                
               delta = xdelta3.encode(old.read(), new.read())
               self.send_response(200)
               self.send_header('Content-type','')
               self.end_headers()
 
-              # buffer = BytesIO(base64.b64encode(delta)).read()
-              # before = delta
-              # mid = base64.b64encode(before)
-              # after = base64.decodebytes(mid)
-
-              # working
               self.wfile.write(base64.b64encode(delta))
-              # wip
-              # self.wfile.write(json.dumps(val).encode('utf-8'))

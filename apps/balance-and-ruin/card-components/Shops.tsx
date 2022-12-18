f9026faa8a4ef75b5ee0159e730c9852c7731caa
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-12-18 12:20:21 -0500

    Add expensive rod/super balls

--- apps/balance-and-ruin/card-components/Shops.tsx
+++ apps/balance-and-ruin/card-components/Shops.tsx
@@ -1,15 +1,28 @@
 import { Card } from "@ff6wc/ui";
 import { CardColumn } from "~/components/CardColumn/CardColumn";
 import { FlagSwitch } from "~/components/FlagSwitch/FlagSwitch";
+import { Divider } from "~/design-components/Divider/Divider";
 
 export const Shops = () => {
   return (
     <Card title={"Shops"}>
       <CardColumn>
-        <FlagSwitch flag="-npi" label="No Priceless Items" />
         <FlagSwitch flag="-snbr" label="No Breakable Rods" />
-        <FlagSwitch flag="-snes" label="No Elemental Shields" />
+        <FlagSwitch
+          flag="-sebr"
+          helperText="Increases the base price of Poison, Fire, Ice, Thunder, Gravity, and Pearl Rods"
+          label="Expensive Breakable Rods"
+        />
+        <Divider />
         <FlagSwitch flag="-snsb" label="No Super Balls" />
+        <FlagSwitch
+          flag="-sesb"
+          helperText="Increases the base price of Super Balls"
+          label="Expensive Super Balls"
+        />
+        <Divider />
+        <FlagSwitch flag="-npi" label="No Priceless Items" />
+        <FlagSwitch flag="-snes" label="No Elemental Shields" />
         <FlagSwitch flag="-snee" label="No Exp. Eggs" />
         <FlagSwitch flag="-snil" label="No Illuminas" />
       </CardColumn>

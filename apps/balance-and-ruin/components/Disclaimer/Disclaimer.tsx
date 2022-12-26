b59c2257a7ba6abe6cb91c48974ca652c3a18316
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-12-26 13:40:25 -0500

    update disclaimer

--- apps/balance-and-ruin/components/Disclaimer/Disclaimer.tsx
+++ apps/balance-and-ruin/components/Disclaimer/Disclaimer.tsx
@@ -2,8 +2,8 @@ export type DisclaimerProps = Record<string, unknown>;
 
 export const Disclaimer = ({}: DisclaimerProps) => {
   return (
-    <div className="flex flex-col w-full min-h-[75px] justify-center items-center bg-gray-600 text-white text-xs">
-      <p>Created by AtmaTek and </p>
+    <div className="flex flex-col gap-2 w-full min-h-[75px] justify-center items-center bg-gray-600 text-white text-xs">
+      <p>Created by AtmaTek and maintained by the community</p>
       <p>
         Final Fantasy VI: Worlds Collide is an unofficial fan project not
         affiliated in any way with Square Enix

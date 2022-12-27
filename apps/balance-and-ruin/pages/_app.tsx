9beea2578d4adf13ee641db09d021963da0c0e20
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-12-26 20:22:31 -0500

    try Roboto

--- apps/balance-and-ruin/pages/_app.tsx
+++ apps/balance-and-ruin/pages/_app.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import { wrapper } from "~/state/store";
 
-import { Montserrat } from "@next/font/google";
+import { Montserrat, Roboto } from "@next/font/google";
 import { cx } from "cva";
 import { AppType } from "next/app";
 import { Schema } from "~/state/schemaSlice";
@@ -12,6 +12,7 @@ import "~/styles/globals.css";
 const client = new QueryClient({});
 
 const montserrat = Montserrat();
+const roboto = Roboto({ weight: ["500", "700", "400"] });
 
 type Props = {
   schema: Schema;
@@ -20,7 +21,7 @@ type Props = {
 const App: AppType<Props> = ({ Component, ...rest }: AppProps<Props>) => {
   const { store, props } = wrapper.useWrappedStore(rest);
   return (
-    <div className={cx(montserrat.className, "bg-white text-grey")}>
+    <div className={cx(roboto.className, "bg-white text-grey")}>
       <Provider store={store}>
         <QueryClientProvider client={client}>
           <Component {...props.pageProps} />

0aa944d6c42535dabceedbcd9e47b858b8a751e4
Kiel <95580337+kielbasiago@users.noreply.github.com>
2023-01-13 17:50:17 -0500

    update shared next-config

--- apps/balance-and-ruin/next.config.js
+++ apps/balance-and-ruin/next.config.js
@@ -1,12 +1,6 @@
+const config = require('@ff6wc/next-config')
 /** @type {import('next').NextConfig} */
+
 module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    fontLoaders: [
-      { loader: "@next/font/google", options: { subsets: ["latin"] } },
-    ],
-  },
-  
-  transpilePackages: ["@ff6wc/ff6-types", "@ff6wc/ui", "@ff6wc/utils", "@ff6wc/tailwind-config"],
+  ...config
 };

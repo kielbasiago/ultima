31fdbc6e7c0e08e08fe1db197dce7105f0499061
Kiel <95580337+kielbasiago@users.noreply.github.com>
2023-01-14 00:22:25 -0500

    add cursed shield battles

--- apps/balance-and-ruin/card-components/OtherItems.tsx
+++ apps/balance-and-ruin/card-components/OtherItems.tsx
@@ -1,11 +1,13 @@
 import { Card } from "@ff6wc/ui";
 import { CardColumn } from "~/components/CardColumn/CardColumn";
+import { FlagRange } from "~/components/FlagRange/FlagRange";
 import { FlagSwitch } from "~/components/FlagSwitch/FlagSwitch";
 
 export const OtherItems = () => {
   return (
     <Card title={"Other"}>
       <CardColumn>
+        <FlagRange flag="-csb" label="Cursed Shield Battles" />
         <FlagSwitch
           flag="-saw"
           label="Stronger Atma Weapon"

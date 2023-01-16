2a0c35aa26eeae8a7ba46cde0615de075c08bc28
Kiel <95580337+kielbasiago@users.noreply.github.com>
2023-01-16 09:18:31 -0500

    Add Beta Presets

--- apps/balance-and-ruin/page-components/BetaPage.tsx
+++ apps/balance-and-ruin/page-components/BetaPage.tsx
@@ -1,10 +1,12 @@
 import { BetaCard } from "~/card-components/BetaCard";
+import { BetaPresets } from "~/card-components/BetaPresetsCard";
 import { WorkshopCard } from "~/card-components/WorkshopCard";
 import { PageContainer } from "~/components/PageContainer/PageContainer";
 
 export const BetaPage = () => {
   return (
     <PageContainer columns={1}>
+      <BetaPresets />
       <BetaCard />
       <WorkshopCard />
     </PageContainer>

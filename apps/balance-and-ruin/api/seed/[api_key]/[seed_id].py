3e6df0589b6cf4093e5248883724d1cc9bf05150
asilverthorn <96998881+asilverthorn@users.noreply.github.com>
2023-07-16 17:52:32 -0600

    storing and retrieving from postgres

--- apps/balance-and-ruin/api/seed/[api_key]/[seed_id].py
+++ apps/balance-and-ruin/api/seed/[api_key]/[seed_id].py
@@ -40,7 +40,6 @@ class handler(BaseHTTPRequestHandler):
     from api_utils.collections import PATCHES, SEEDS, SPOILER_LOGS
     log = SeedStorage.get_spoiler_log(seed_id)
     patch = SeedStorage.get_patch(seed_id)
-    seed = SeedStorage.get_seed(seed_id)
     
     from api_utils.get_seed_payload import get_seed_payload
     from api_utils.get_seed_filename import get_seed_filename
